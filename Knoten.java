473d685d4ec59e5c6a7c2bd5800c326cf1bb5911
Sabine <11sloe@itg-dah.de>
2018-11-17 15:58:02 +0100

    Gemeinsames Projekt angelegt

---
+++ Knoten.java
@@ -0,0 +1,36 @@
+
+public class Knoten extends Listenelement
+{
+    private Kiste inhalt;
+    private Listenelement naechster;
+
+    public Knoten(Kiste inhalt_)
+    {
+        
+    }
+
+    public void setNaechster(Listenelement elem)
+    {
+        
+    }
+
+    public void ausgeben()
+    {
+        
+    }
+
+    public boolean istLeer()
+    {
+        return false;
+    }
+
+    public  Kiste getInhalt()
+    {
+        return inhalt;
+    }
+
+    public Listenelement getNaechster()
+    {
+        return naechster;
+    }
+}
